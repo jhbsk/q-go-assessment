8922fa78c77dc40241e73efb2382a3c9df348abe
Jasdeep <jhbsk@users.noreply.github.com>
2017-11-15 15:22:00 +0000

    Updating readme and header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.png';
 import './styles.css';
 
 const Header = () => {
   return (
     <div className="header-title">
-      <img src={logo} className="header-logo" alt="logo" />
-      <h2>QGo Desktop React Assessment</h2>
+      <h2>Todo in React</h2>
     </div>
   );
 };
